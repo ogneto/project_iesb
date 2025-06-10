4a0c679bfd6aec4000a475e5a02475071fc1a6e3
OgNeto <ogsilvanetu@gmail.com>
2025-06-10 20:09:04 -0300

    Configuração Inicial do Projeto

--- src/teachers/entities/teacher.entity.ts
+++ src/teachers/entities/teacher.entity.ts
@@ -1,1 +1,25 @@
-export class Teacher {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Teacher {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 40 })
+  email: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
