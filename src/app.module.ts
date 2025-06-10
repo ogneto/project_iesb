9666cb75190f068c15a04b5ed9bf33d4f6971b8d
OgNeto <ogsilvanetu@gmail.com>
2025-06-10 20:09:04 -0300

    Configuração Inicial do Projeto

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT') ?? '5432', 10),
+        database: config.get('DB_NAME'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
