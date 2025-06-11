966a876e7524bbf64a6007eb551dce0946a49558
OgNeto <ogsilvanetu@gmail.com>
2025-06-10 21:58:15 -0300

    Adding TypeOrmModule in the courses module

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,1 +1,25 @@
-export class Course {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Course {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  course_name: string;
+
+  @Column({ type: 'varchar', length: 200 })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
