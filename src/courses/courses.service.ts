632145a1d769562f75f729c6c32419f3c40b47a1
OgNeto <ogsilvanetu@gmail.com>
2025-06-10 22:25:04 -0300

    I implemented the first course crud

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -14,12 +14,16 @@ export class CoursesService {
 
   notFound() {
     throw new NotFoundException(
-      `Sorry. I can't find this student. Please try again.`,
+      `Sorry. I can't find this course. Please try again.`,
     );
   }
 
-  create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+  async create(createCourseDto: CreateCourseDto) {
+    const course = {
+      course_name: createCourseDto.course_name,
+      description: createCourseDto.description,
+    };
+    return await this.courseRepository.save(course);
   }
 
   async findAll() {
@@ -40,11 +44,28 @@ export class CoursesService {
     return course;
   }
 
-  update(id: string, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+  async update(id: string, updateCourseDto: UpdateCourseDto) {
+    const updatedCourse = {
+      course_name: updateCourseDto?.course_name,
+      description: updateCourseDto?.description,
+    };
+    const course = await this.courseRepository.preload({
+      id,
+      ...updatedCourse,
+    });
+    if (!course) {
+      return this.notFound();
+    }
+    return await this.courseRepository.save(course);
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} course`;
+  async remove(id: string) {
+    const course = await this.courseRepository.findOneBy({
+      id,
+    });
+    if (!course) {
+      return this.notFound();
+    }
+    return await this.courseRepository.remove(course);
   }
 }
--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCourseDto {}
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
+  @MaxLength(50)
+  course_name: string;
+
+  @IsString()
+  @MaxLength(200)
+  description: string;
+}
